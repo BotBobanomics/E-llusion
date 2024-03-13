f66f3a6f18aed60997a6ca3c8e4efe11f39aac84
lmurayam <lmurayam@ucsc.edu>
2024-03-12 20:27:17 -0700

    walljump velocity limit

--- Assets/Scipts/WallJump.cs
+++ Assets/Scipts/WallJump.cs
@@ -8,6 +8,7 @@ public class WallJump : MonoBehaviour
     [SerializeField] float xAmount;
     [SerializeField] float yAmount;
     [SerializeField] float mult;
+    [SerializeField] float velocityLimit = 20f;
     private Vector3 wallNormal;
     void OnCollisionStay(Collision other){
         //While touching wall, calculate normal of contact point and push in the opposite direction when space is pushed. 
@@ -17,7 +18,7 @@ public class WallJump : MonoBehaviour
                 //Debug.Log(wallNormal);
                 Vector3 jumpDirection = (wallNormal * xAmount)+(Vector3.up * yAmount);
                 jumpDirection = jumpDirection.normalized;
-                PlayerManager.Instance.pushPlayer(mult,jumpDirection,20f);
+                PlayerManager.Instance.pushPlayer(mult,jumpDirection,velocityLimit);
             }
         }
     }
