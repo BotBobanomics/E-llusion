ef0c514d87f5bb6c47441bb44ec31701a533c88c
lmurayam <lmurayam@ucsc.edu>
2024-03-12 21:26:33 -0700

    Intro Text start

---
+++ Assets/Scipts/IntroText.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class IntroText : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    [SerializeField] float delayFade;
+    [SerializeField] float fadeSpeed;
+    TextMeshProUGUI text;
+    void Start()
+    {
+        text=GetComponent<TextMeshProUGUI>();
+        StartCoroutine(FadeText(delayFade,fadeSpeed));
+    }
+
+    IEnumerator FadeText(float delay,float speed){
+        yield return new WaitForSeconds(delay); 
+        float alpha = 100f;
+        Color32 textColor = text.color;
+        Color32 clearColor = Color.clear;
+        while(alpha>0){
+            alpha -= speed * Time.deltaTime;
+            text.color = Color32.Lerp(textColor,clearColor,alpha/100f);
+            Debug.Log((alpha,alpha/100f));
+            yield return null;
+        }
+    }
+}
