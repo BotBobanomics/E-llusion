cd05c18cd98fcf913de063db4a7dd82aaf7b3167
Nhat Thai <100763926+nhaticus@users.noreply.github.com>
2024-03-12 19:36:19 -0700

    move testing stuff out

---
+++ Assets/Scipts/EnemyBehavior.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    [Header("Damage to Player")]
+    public int damage;
+
+    private void Awake()
+    {
+        // setting the variable without manual work
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("PlayerObj").transform;
+    }
+
+    void Update()
+    {
+        // chase player
+        agent.SetDestination(player.position);
+    }
+    void OnCollisionEnter(Collision collision)  // colliding with a player object
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);    //  destroys the enemy
+            // damage is set within the inspector
+            GameManager.PlayerHealth -= damage;
+        }
+    }
+}
